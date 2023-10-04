227ffa4d541ef69efd59ef86d24a0addca3c02a4
isel06002 <isel06002@E-PW0234HX>
2023-10-04 10:16:56 +0200

    Add project files.

---
+++ Inlämning 3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Inlämning_3.__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du");
+            int gammal = int.Parse(Console.ReadLine());
+            if(gammal >=16 && gammal<=19)
+            {
+                Console.WriteLine("Du får delta i tävlingen");
+            }
+            else if (gammal < 16)
+            {
+                Console.WriteLine("Du är för ung för att delta");
+            }
+            else if (gammal >19)
+            {
+                Console.WriteLine("Du är för gammal för att delta");
+            }
+        }   
+    }
+}
